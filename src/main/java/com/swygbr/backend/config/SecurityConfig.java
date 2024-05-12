b3c08738a0b7b0ecbc2399a1908e371fb1f4e146
jskim2376 <jskim2376@gmail.com>
2024-05-12 15:05:28 +0900

    chore: h2-console 설정 변경.
    - /h2-console path는 spring security에서 예외 처리
    - application-local.properties가 활성화 시에만 h2-console 설정 적용됨

---
+++ src/main/java/com/swygbr/backend/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.swygbr.backend.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    @Profile("local")
+    public WebSecurityCustomizer localConfig() {
+        return (web) -> web.ignoring().requestMatchers(PathRequest.toH2Console());
+    }
+
+}
