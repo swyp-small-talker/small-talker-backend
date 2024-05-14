6db3287ae74012a093afe6a1cff4c1203ac77e3f
jskim2376 <jskim2376@gmail.com>
2024-05-14 16:20:47 +0900

    feat: tutorial service에 상태 조회 기능 추가
    - service 기능 작성
    - dto 작성

--- src/main/java/com/swygbr/backend/tutorial/service/TutorialService.java
+++ src/main/java/com/swygbr/backend/tutorial/service/TutorialService.java
@@ -1,119 +1,41 @@
 package com.swygbr.backend.tutorial.service;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.swygbr.backend.tutorial.domain.TutorialEntity;
-import com.swygbr.backend.tutorial.dto.RequestTutorialInsertDto;
-import com.swygbr.backend.tutorial.dto.TutorialCompletionDto;
 import com.swygbr.backend.tutorial.dto.TutorialDto;
-import com.swygbr.backend.tutorial.enums.BotType;
-import com.swygbr.backend.tutorial.enums.InsertType;
-import com.swygbr.backend.tutorial.enums.TutorialType;
+import com.swygbr.backend.tutorial.dto.TutorialStatusDto;
 import com.swygbr.backend.tutorial.repository.TutorialRepository;
 import com.swygbr.backend.user.domain.UserEntity;
 import com.swygbr.backend.user.repository.UserRepository;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RequiredArgsConstructor
+@Slf4j
 @Transactional
 @Service
 public class TutorialService {
     private final TutorialRepository tutorialRepository;
     private final UserRepository userRepository;
 
-    public TutorialCompletionDto checkCompletion() {
-        UserEntity userEntity = getUserFromEmail();
-        BotType completedByBotType = userEntity.getCompletedByBotType();
+    public List<TutorialStatusDto> findTutorialStatus() {
+        UserEntity userEntity = getDefaultUser();
+        List<TutorialEntity> entities = tutorialRepository.findByIsStartTrue();
 
-        return TutorialCompletionDto.builder()
-                .genieComplete(BotType.GENIE == completedByBotType)
-                .jasmineComplete(BotType.JASMINE == completedByBotType)
-                .build();
+        return entities.stream()
+                .map((entity) -> TutorialStatusDto.fromEntity(userEntity, entity))
+                .toList();
     }
 
-    public TutorialDto findTutorialStartMessage(BotType botType) {
-        UserEntity userEntity = getUserFromEmail();
-
-        if (userEntity.getCompletedByBotType() == botType) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "이미 완료한 튜토리얼 입니다.");
-        }
-
-        TutorialEntity entity = tutorialRepository.findByParentIsNullAndBotType(botType)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "튜토리얼을 찾을 수 없습니다."));
-
-        return TutorialDto.fromEntity(entity);
+    private UserEntity getDefaultUser() {
+        return userRepository.findById(1L)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "기본 유저를 찾을 수 없습니다."));
     }
-
-    public TutorialDto findTutorial(long tutorialId) {
-        UserEntity userEntity = getUserFromEmail();
-
-        var entity = tutorialRepository.findById(tutorialId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "튜토리얼을 찾을 수 없습니다."));
-
-        if (userEntity.getCompletedByBotType().isHigherThan(entity.getBotType())) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "이미 완료한 튜토리얼 입니다.");
-        }
-
-        var dto = TutorialDto.fromEntity(entity);
-        dto.setChildId(entity.getChildren().get(0).getId());
-        return dto;
-    }
-
-    public TutorialDto postTutorialChoice(long tutorialId) {
-        UserEntity userEntity = getUserFromEmail();
-
-        var entity = tutorialRepository.findById(tutorialId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "튜토리얼을 찾을 수 없습니다."));
-
-        if (userEntity.getCompletedByBotType().isHigherThan(entity.getBotType())) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "이미 완료한 튜토리얼 입니다.");
-        }
-
-        if (entity.getTutorialType() != TutorialType.CHOICE) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "튜토리얼 선택지 타입이 아닙니다.");
-        }
-
-        // 마지막 선택지면 user에게 user 카드 부여
-        if (entity.getUserCard() != null) {
-            userEntity.assignUserCard(entity.getUserCard());
-        }
-
-        // 다음 튜토리얼 조회
-        var child = entity.getChildren().get(0);
-        return TutorialDto.fromEntity(child);
-    }
-
-    public TutorialDto postTutorialInsertUserName(long tutorialId, RequestTutorialInsertDto request) {
-        UserEntity userEntity = getUserFromEmail();
-        var entity = tutorialRepository.findById(tutorialId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "튜토리얼을 찾을 수 없습니다."));
-
-        if (userEntity.getCompletedByBotType().isHigherThan(entity.getBotType())) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "이미 완료한 튜토리얼 입니다.");
-        }
-
-        if (entity.getTutorialType() != TutorialType.INSERT) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "튜토리얼 입력 타입이 아닙니다.");
-        }
-
-        if (InsertType.USER_NAME == request.insertType()) {
-            userEntity.setUserName(request.content());
-        }
-
-        // 다음 튜토리얼 조회
-        var child = entity.getChildren().get(0);
-        return TutorialDto.fromEntity(child);
-    }
-
-    private UserEntity getUserFromEmail() {
-        String email = SecurityContextHolder.getContext().getAuthentication().getName();
-        return userRepository.findByEmail(email)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "존재하지 않는 email입니다: " + email));
-    }
-
 }
