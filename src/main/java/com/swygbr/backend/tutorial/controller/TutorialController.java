6cb3e2d336f060ca8bd5421d2239d31210041cad
jskim2376 <jskim2376@gmail.com>
2024-05-14 21:47:52 +0900

    feat: tutorial controller에 input api 추가
    - userName 변경하는 기능 api 개발

--- src/main/java/com/swygbr/backend/tutorial/controller/TutorialController.java
+++ src/main/java/com/swygbr/backend/tutorial/controller/TutorialController.java
@@ -4,12 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.swygbr.backend.tutorial.dto.RequestTutorialInputDto;
 import com.swygbr.backend.tutorial.service.TutorialService;
-
-import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/tutorial")
@@ -30,26 +31,11 @@ public class TutorialController {
     return ResponseEntity.ok(tutorialService.findTutorialById(tutorialId));
   }
 
-  // // 튜토리얼 채팅 시작
-  // @GetMapping("/chat/{chatId}")
-  // public ResponseEntity<?> getTutorialStart(@PathVariable BotType botType) {
-  // return ResponseEntity.ok(tutorialService.findTutorialStartMessage(botType));
-  // }
-
-  // // 튜토리얼 선택지 제출
-  // @PostMapping("/{tutorialId}/choice")
-  // public ResponseEntity<?> postTutorialSubmit(@PathVariable BotType botType,
-  // @PathVariable long tutorialId) {
-  // return ResponseEntity.ok(tutorialService.postTutorialChoice(tutorialId));
-  // }
-
-  // // 튜토리얼 입력 제출
-  // @PostMapping("/{tutorialId}/insert")
-  // public ResponseEntity<?> postTutorialInsert(@PathVariable long tutorialId,
-  // @RequestBody RequestTutorialInsertDto request) {
-  // BotType botType = BotType.GENIE;
-  // return
-  // ResponseEntity.ok(tutorialService.postTutorialInsertUserName(tutorialId,
-  // request));
-  // }
+  // 튜토리얼 입력
+  @PostMapping("/input")
+  public ResponseEntity<?> postInput(@RequestBody RequestTutorialInputDto request) {
+    tutorialService.submitInput(request);
+    return ResponseEntity.ok().build();
+  }
+  
 }
