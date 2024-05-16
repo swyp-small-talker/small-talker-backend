95364b7e9b5c9131a7fc6e623c3e3d911424bfbf
jskim2376 <jskim2376@gmail.com>
2024-05-16 21:38:08 +0900

    feat: tutorial schema 작성

--- src/main/resources/h2/data.sql
+++ src/main/resources/h2/data.sql
@@ -1,1 +1,36 @@
-INSERT INTO `user_card` (`picture`, `title`, `content`) VALUES ('http://example.com/picture.png', '묵언수행형', '말이 적은편');
+INSERT INTO Users(email, name)
+VALUES('test@gmail.com', 'myname');
+INSERT INTO UserCard (picture, title, content)
+VALUES (
+        'http://example.com/picture.png',
+        '리액션봇형',
+        '상대방 말에 공감만 해주다가 정작 내 이야기는 못하고 끝나버리죠.'
+    );
+INSERT INTO UserCard (picture, title, content)
+VALUES (
+        'http://example.com/picture.png',
+        '리액션봇형',
+        '상대방 말에 공감만 해주다가 정작 내 이야기는 못하고 끝나버리죠.'
+    );
+INSERT INTO UserCard (picture, title, content)
+VALUES (
+        'http://example.com/picture.png',
+        '투머치토커형',
+        '하고 싶은 말이 너무 많아서 돌고 돌아 내 이야기만 하고 있는 경우가 많죠.'
+    );
+INSERT INTO UserCard (picture, title, content)
+VALUES (
+        'http://example.com/picture.png',
+        '질문폭격기형',
+        '궁금한 게 너무 많아서 꼬리에 꼬리를 무는 질문을 하곤 하죠.'
+    );
+INSERT INTO UserCard (picture, title, content)
+VALUES (
+        'http://example.com/picture.png',
+        '스몰토커형',
+        '완성형 토크 플레이어 말이 필요 없는 센스를 지니고 계시군요!'
+    );
+INSERT INTO Tutorial(tutorialType, isStart)
+VALUES ('PROFILE', true);
+INSERT INTO Tutorial(tutorialType, isStart)
+VALUES ('USER_CARD', true);
--- src/main/resources/h2/schema.sql
+++ src/main/resources/h2/schema.sql
@@ -1,18 +1,45 @@
-CREATE TABLE `user_card` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `picture` varchar(255) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `content` varchar(255) NOT NULL,
-  `createdAt` timestamp NOT NULL DEFAULT now(),
-  `updatedAt` timestamp
+CREATE TABLE UserCard (
+  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  picture VARCHAR(255) NOT NULL,
+  title VARCHAR(20) NOT NULL,
+  content VARCHAR(255) NOT NULL
 );
-
-CREATE TABLE `tutorial` (
-  `id` int NOT NULL,
-  `fk_parent` int NULL,
-  `fk_user_card` int NULL,
-  `character` varchar(255) NOT NULL COMMENT '지니, 회원',
-  `type` varchar(255) NOT NULL COMMENT 'common, choice, reaction',
-  `title` varchar(255) NOT NULL,
-  `content` varchar(255) NOT NULL
+CREATE TABLE Users (
+  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  userCardFk BIGINT DEFAULT NULL,
+  completeProfileTypeTutorial BOOLEAN DEFAULT false NOT NULL,
+  completeUserCardTypeTutorial BOOLEAN DEFAULT false NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  createdAt DATETIME DEFAULT NOW () NOT NULL,
+  updatedAt DATETIME DEFAULT NOW () ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+  FOREIGN KEY (userCardFk) REFERENCES UserCard (id)
+);
+CREATE TABLE Tutorial (
+  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  tutorialType VARCHAR(10) NOT NULL,
+  isStart BOOLEAN DEFAULT false NOT NULL
+);
+CREATE TABLE TutorialMessage (
+  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  tutorialFk BIGINT,
+  messageType VARCHAR(10),
+  FOREIGN KEY (tutorialFk) REFERENCES Tutorial (id)
+);
+CREATE TABLE TutorialMessageText (
+  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  messageFk BIGINT UNIQUE,
+  actorType VARCHAR(10) NOT NULL,
+  textType VARCHAR(10),
+  content VARCHAR(255) NOT NULL,
+  FOREIGN KEY (messageFk) REFERENCES TutorialMessage (id)
+);
+CREATE TABLE TutorialMessageChoice (
+  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  messageFk BIGINT NOT NULL,
+  reactionTutorialFk BIGINT UNIQUE NOT NULL,
+  choiceType VARCHAR(10) NOT NULL,
+  content VARCHAR(255) NOT NULL,
+  FOREIGN KEY (messageFk) REFERENCES TutorialMessage (id),
+  FOREIGN KEY (reactionTutorialFk) REFERENCES Tutorial (id)
 );
