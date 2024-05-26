87dc19713b4d3cb4dae17034f0f24606017cf633
jskim2376 <jskim2376@gmail.com>
2024-05-26 17:28:16 +0900

    chore: security config 설정 통일, 세션 미사용 설정

--- src/main/java/com/swygbr/backend/config/SecurityConfig.java
+++ src/main/java/com/swygbr/backend/config/SecurityConfig.java
@@ -2,12 +2,11 @@ package com.swygbr.backend.config;
 
 import java.util.Arrays;
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -25,56 +24,23 @@ public class SecurityConfig {
     private final JwtAuthFilter jwtCookieFilter;
 
     @Bean
-    @Profile("local")
     public SecurityFilterChain localFilterChain(HttpSecurity http) throws Exception {
         http
                 .formLogin(formLogin -> formLogin.disable())
                 .httpBasic(httpBasic -> httpBasic.disable())
                 .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .cors((cors) -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/h2-console/**").permitAll()
                         .requestMatchers("/login/**").permitAll()
-                        .requestMatchers("/token/**").permitAll()
                         .requestMatchers("/user-card/small-talker").authenticated()
                         .requestMatchers("/user-card/**").permitAll()
                         .requestMatchers("/tutorial/**").authenticated()
                         .requestMatchers("/practice/**").authenticated()
                         .anyRequest().authenticated())
                 .addFilterBefore(jwtCookieFilter, UsernamePasswordAuthenticationFilter.class);
-
-        return http.build();
-    }
-
-    @Bean
-    @Profile("dev")
-    public SecurityFilterChain devFilterChain(HttpSecurity http) throws Exception {
-        http
-                .formLogin(formLogin -> formLogin.disable())
-                .httpBasic(httpBasic -> httpBasic.disable())
-                .csrf(csrf -> csrf.disable())
-                .cors((cors) -> cors.configurationSource(
-                        corsConfigurationSource()))
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/user-card/**").permitAll()
-                        .requestMatchers("/tutorial/**").permitAll()
-                        .anyRequest().authenticated());
-        return http.build();
-    }
-
-    @Bean
-    @Profile("prod")
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .formLogin(formLogin -> formLogin.disable())
-                .httpBasic(httpBasic -> httpBasic.disable())
-                .csrf(csrf -> csrf.disable())
-                .cors((cors) -> cors.configurationSource(
-                        corsConfigurationSource()))
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/user-card/**").permitAll()
-                        .requestMatchers("/tutorial/**").authenticated()
-                        .anyRequest().authenticated());
         return http.build();
     }
 
